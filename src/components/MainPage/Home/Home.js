a07539c1ebd10c6e362a5d755dfb63ec26621a9e
dimorm <dimorm@gmail.com>
2019-04-17 04:12:38 +0300

    home final before animation

--- src/components/MainPage/Home/Home.js
+++ src/components/MainPage/Home/Home.js
@@ -3,7 +3,7 @@ import AnimatedTitle from './AnimatedTitle.js';
 import './Home.css';
 import * as CONSTANTS from './../../../constants';
 import Carusel from './Carusel.js';
-
+import logo from './images/shamir.png';
 
 
 
@@ -40,27 +40,37 @@ this.setState({currentImgPath:CONSTANTS.MAIN_PAGE_HOME_ARRAY_IMAGES[pos]})
 		return(
 
 
-		<div className='home'> 
+		<div className='home-main-container'> 
 
-			<div className='grid'>	
-				<div className='white-block'></div>
-				<div className='animated'></div>
-			</div>
-			<div className='text-desc'> <p>{CONSTANTS.MAIN_PAGE_HOME_TEXT}</p></div>
-			<div className='skipper_car'><Carusel changeImage={this.changeImage}/></div>
-			
-			{console.log(this.state.currentImgPath)}
 
-			
-			<div className='img-show' style={{backgroundImage: `url(${require(`${this.state.currentImgPath}`)})`}}                              />
+					<div className='home-inner-container'> 
+
+						<div className='home-main-div-grid'>	
+
+							<div className='home-white-block-animated'></div>
+							<div className='home-logo-animated'><img src={logo} /></div>
+
+						</div>
 
 
 
+					<div className='home-text-div'>
 
-	
+						<p>{CONSTANTS.MAIN_PAGE_HOME_TEXT}</p>
+
+						<div className='home-carusel-div'><Carusel changeImage={this.changeImage}/></div>
+
+					</div>
+
+
+					<div className='home-image-div' style={{backgroundImage: `url(${require(`${this.state.currentImgPath}`)})`}}></div>      	
+
+					</div>
 
 		</div>
 
+	
+	
 		);
 
 	}
