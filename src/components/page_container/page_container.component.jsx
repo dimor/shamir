4a9ada41f27f8d361eccc46cc6e2bfcd0acf76fd
Dima Max <dimorm@gmail.com>
2020-05-15 19:58:55 +0300

    optimization

--- src/components/page_container/page_container.component.jsx
+++ src/components/page_container/page_container.component.jsx
@@ -1,9 +1,9 @@
-import React ,{useState ,useEffect} from 'react';
+import React from 'react';
 import './page_container.styles.css';
 import HomePage from './pages/home.page/home.page.component';
 import AboutPage from './pages/about.page/about.page.component';
 import ProductsPage from './pages/products.page/products.page.component';
-import {BrowserRouter as Router,Switch, Route} from "react-router-dom";
+import {Switch, Route} from "react-router-dom";
   
 
 
