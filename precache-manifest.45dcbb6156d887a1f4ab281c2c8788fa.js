9ecb665462cf569e26f258fff6d64518e39cad1e
Dima Max <dimorm@gmail.com>
2020-05-15 17:29:58 +0300

    Updates

---
+++ precache-manifest.45dcbb6156d887a1f4ab281c2c8788fa.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "22a1512d123751b700df",
+    "url": "/shamir/static/css/main.8f6f2013.chunk.css"
+  },
+  {
+    "revision": "22a1512d123751b700df",
+    "url": "/shamir/static/js/main.16577cab.chunk.js"
+  },
+  {
+    "revision": "954c545cbdc1f5e0f295",
+    "url": "/shamir/static/js/runtime~main.a2eb5e3b.js"
+  },
+  {
+    "revision": "8d37aec433054c57a4b8",
+    "url": "/shamir/static/js/2.11430531.chunk.js"
+  },
+  {
+    "revision": "e91eebd0c4f8098e18f3e55d12dee168",
+    "url": "/shamir/static/media/chair.e91eebd0.png"
+  },
+  {
+    "revision": "402944be28efc18cd1c5df8cf6eabaf9",
+    "url": "/shamir/static/media/img1.402944be.png"
+  },
+  {
+    "revision": "84205c329c4a2e203a6b3393248b7a13",
+    "url": "/shamir/static/media/img2.84205c32.png"
+  },
+  {
+    "revision": "2afd3ae825bac7204d927b46a654a500",
+    "url": "/shamir/static/media/img3.2afd3ae8.png"
+  },
+  {
+    "revision": "d936ce8b631ae88c91c3c912e08adce7",
+    "url": "/shamir/static/media/shamir.d936ce8b.png"
+  },
+  {
+    "revision": "4f87917e5db5cd165e70b9956aff7da5",
+    "url": "/shamir/static/media/beds.4f87917e.png"
+  },
+  {
+    "revision": "9fafdbfeb7e1cdf1fda225a67ddec384",
+    "url": "/shamir/static/media/benches.9fafdbfe.png"
+  },
+  {
+    "revision": "b48bf3e9056204a70f225d4ae07e39cb",
+    "url": "/shamir/static/media/chairs.b48bf3e9.png"
+  },
+  {
+    "revision": "0f3d17406a05b7bc5f297603214df692",
+    "url": "/shamir/static/media/closets.0f3d1740.png"
+  },
+  {
+    "revision": "8612999784caf5030e77444f40de948d",
+    "url": "/shamir/static/media/counters.86129997.png"
+  },
+  {
+    "revision": "9f24fa23f9f080a6e7a82e3f1e4d722c",
+    "url": "/shamir/static/media/partitions.9f24fa23.png"
+  },
+  {
+    "revision": "98067313821a5a8bf9aa633e66e2f737",
+    "url": "/shamir/static/media/podiums.98067313.png"
+  },
+  {
+    "revision": "2099700c8288c873d4045e47df757e8f",
+    "url": "/shamir/static/media/tables.2099700c.png"
+  },
+  {
+    "revision": "614a25b379463574f7c9c934070a0a89",
+    "url": "/shamir/static/media/title.614a25b3.png"
+  },
+  {
+    "revision": "c80d64569ca2f89135176546d5c0e4c4",
+    "url": "/shamir/index.html"
+  }
+];
